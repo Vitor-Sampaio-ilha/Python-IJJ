802a8dcb2e47c8b81fcc4a55717a58010a376094
Vitor Sampaio <vitorisf56@gmail.com>
2024-12-08 20:32:15 -0300

    Calculadora.py

--- Aulas de Python/Aula06_Estruturas_Repetição_Iteração/Calculadora.py
+++ Aulas de Python/Aula06_Estruturas_Repetição_Iteração/Calculadora.py
@@ -2,17 +2,24 @@ operacao = input("Digite a operação (soma, subtracao, multiplicacao, divisao):
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
+# Verifica qual operação foi escolhida
 match operacao:
     case 'soma':
         resultado = num1 + num2
+        print(f"Resultado da soma: {resultado}")
     case 'subtracao':
         resultado = num1 - num2
+        print(f"Resultado da subtração: {resultado}")
     case 'multiplicacao':
         resultado = num1 * num2
+        print(f"Resultado da multiplicação: {resultado}")
     case 'divisao':
-        resultado = num1 / num2        
+        # Evita divisão por zero
+        if num2 != 0:
+            resultado = num1 / num2
+            print(f"Resultado da divisão: {resultado}")
+        else:
+            print("Erro: Divisão por zero não é permitida.")
     case _:
-        resultado = 'Operação inválida'
-
-
-print(f"Resultado da {operacao}: {resultado}")
+        # Mensagem para operação inválida
+        print("Operação inválida. Tente novamente.")
